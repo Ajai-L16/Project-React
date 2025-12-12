7c58451cb4d50a2b6d98dbc13e9edb78dae3e104
Ajai <ajailight7646337@gmail.com>
2025-12-12 12:26:08 +0530

    3rd commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,58 @@
+import React, { useEffect, useState } from "react";
 import "../Styles/Home.css";
 
 function Home() {
-  return <h1>Home Page</h1>;
+  const [assignmentData, setAssignmentData] = useState([]);
+
+  useEffect(() => {
+    fetch("/users.json")
+      .then((res) => res.json())
+      .then((data) => setAssignmentData(data))
+      .catch((err) => console.error("Failed to load JSON:", err));
+  }, []);
+
+  const pendingCount = assignmentData.filter(
+    (assignment) => assignment.status === "Pending"
+  ).length;
+  const recentAssignments = [...assignmentData].reverse().slice(0, 6);
+
+  return (
+    <div className="home-container1">
+      <div className="break">
+        
+        <h1 className="home_h1">Recent Tasks</h1>
+        <div className="assignments-lists">
+          {recentAssignments.map((assignment) => (
+            <div key={assignment.id} className="assignment-card">
+              <div className="card-header">
+                <div className="header-left">
+                  <h3>{assignment.title}</h3>
+                  <span className="subject-tag">{assignment.subject}</span>
+                </div>
+
+                <div className="header-right">
+                  <span className="due-date">Due: {assignment.dueDate}</span>
+                  <span
+                    className={`status-badge ${assignment.status.toLowerCase()}`}
+                  >
+                    {assignment.status}
+                  </span>
+                </div>
+              </div>
+
+              <div className="card-body">
+                <p>{assignment.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <h1 className="home_h1">Pending</h1>
+      <div className="home-pending">
+        <h3>Assignments pending: {pendingCount}</h3>
+      </div>
+    </div>
+  );
 }
+
 export default Home;
---
+++ src/Pages/Registration.js
@@ -0,0 +1,11 @@
+function Registration(){
+    return(
+        <div className="registration-container">
+            <h1 className="registration-h1">Product</h1>
+            <div className="registration-info">
+                <h2 className="registration-h2">User Information</h2>
+            </div>
+        </div>
+    ); 
+}
+export default Registration;
